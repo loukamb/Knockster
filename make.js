395630d39599a2824ff6156aa4d47112eaec074d
louka <louka.menard.blondin@pm.me>
2025-04-10 11:25:59 -0400

    Oops, forgot to increase ver.

--- make.js
+++ make.js
@@ -8,7 +8,7 @@ const userScriptInfo = `
 // @match       https://knockout.chat/*
 // @grant       GM_getValues
 // @grant       GM_setValues
-// @version     1.2
+// @version     1.3
 // @homepageURL https://github.com/loukamb/Knockster
 // @downloadURL https://github.com/loukamb/Knockster/releases/latest/download/index.user.js
 // @author      https://github.com/loukamb
